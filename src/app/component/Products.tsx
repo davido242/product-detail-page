68edea9ecf1f66aeeccba7b884523f1bbc2de96e
Monday_David_Sarka <davidsarka242@gmail.com>
2024-02-20 17:56:49 +0100

    Added button for cart and detail pages navigation

--- src/app/component/Products.tsx
+++ src/app/component/Products.tsx
@@ -25,11 +25,15 @@ export default function Products({ products }: Props) {
     <div className='max-w-screen-xl mx-auto grid grid-cols1 md:grid-cols-2 xl:grid-cols-4 gap-10 py-10'>
       {products.map((item) => (
         <Link href={"/"} key={item._id}>
-          <div className='border border-gray-300 rounded-md hover:border-gray-950 duration-300'>
+          <div className='border border-gray-300 rounded-md hover:border-gray-950 duration-300 overflow-hidden p-4'>
             <Image src={item?.image} alt={item.title} width={500} height={500} className='w-full h-80 object-cover' />
             <div>
               <p>{item?.title}</p>
-              <p>${item?.price}</p>
+              <p className='font-semibold'>${item?.price}</p>
+              <div className='flex items-center justify-between text-xs text-white py-4'>
+                <button className='bg-gray-500 p-2 rounded-md hover:bg-gray-700'>Add to Cart</button>
+                <button className='bg-gray-500 p-2 rounded-md hover:bg-gray-700'>More info</button>
+              </div>
             </div>
           </div>
         </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Products from "./component/Products";
 
 const getData = async () => {
   const res = await fetch("https://jsonserver.reactbd.com/phone");
-
   if(!res.ok) {
     throw new Error("Failed to fetch data");
   }
